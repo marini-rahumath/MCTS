d032deb1e68b6ffe112c349e94e9e8df71f7a714
AnniyaBK <anniyablue@gmail.com>
2018-09-05 17:03:11 +0800

    Added info button with autoscroll feature (issue: info button is not in the same row as 'Plan Task')

--- imports/ui/pages/tasking.js
+++ imports/ui/pages/tasking.js
@@ -903,6 +903,14 @@ Template.Tasking.events({
         //satNextPass();
     },
 
+    'click .infoPasses' (event, instance){
+        if (coordsPrint.length == 0) {
+            alert("Please plan a task first!");
+        }
+        var elmnt = document.getElementById("viewPasses");
+        elmnt.scrollIntoView();
+    },
+
     'click .editPlanTaskDetails'(event, instance){
 
         popupS.confirm({
